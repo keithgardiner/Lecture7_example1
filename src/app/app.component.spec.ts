2e34d35c89ca1341a13ccd75cc40c83cdda46d49
Keith Gardiner <keith.gardiner@dit.ie>
2023-03-07 09:36:57 +0000

    changed name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Lecture7_Example1'`, () => {
+  it(`should have as title 'Lecture7_example1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Lecture7_Example1');
+    expect(app.title).toEqual('Lecture7_example1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Lecture7_Example1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Lecture7_example1 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lecture7_Example1';
+  title = 'Lecture7_example1';
 }
